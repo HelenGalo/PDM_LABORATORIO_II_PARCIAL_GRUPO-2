1e76440bb911f76927c9ebef580c24f38f307415
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-13 23:12:00 -0600

    intent

--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/RecyclerAdapter.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/RecyclerAdapter.kt
@@ -1,30 +1,39 @@
 package hn.edu.pdm_laboratorio_prueba
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import  android.widget.ImageView
-import com.google.android.material.snackbar.Snackbar
 import androidx.recyclerview.widget.RecyclerView
-
-
+import com.google.android.material.snackbar.Snackbar
 
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private val titles = arrayOf("Registrar Alumno",
-            "Matricula", "Registrar Clase", "Ingresar Notas",
-            "Enviar Matricula", "Enviar Nota")
+    private val titles = arrayOf(
+        "Registrar Alumno",
+        "Matricula", "Registrar Clase", "Ingresar Notas",
+        "Enviar Matricula", "Enviar Nota"
+    )
 
-    private val details = arrayOf("Item detalle 1", "Item detalle 2",
-            "Item detalle 3", "Item detalle 4",
-            "Item detalle 5", "Item detalle 6",
-            "Item detalle 7", "Item detalle 8")
+    private val details = arrayOf(
+        "Item detalle 1", "Item detalle 2",
+        "Item detalle 3", "Item detalle 4",
+        "Item detalle 5", "Item detalle 6",
+        "Item detalle 7", "Item detalle 8"
+    )
 
-    private val images = intArrayOf(R.drawable.image2,
+    private val images = intArrayOf(
+        R.drawable.image2,
         R.drawable.imagen1, R.drawable.clases,
         R.drawable.images3, R.drawable.images4,
-        R.drawable.image5)
+        R.drawable.image5
+    )
+
+    private val context: Context? = null
+
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context)
@@ -45,15 +54,22 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
             itemView.setOnClickListener{ v: View ->
                 var position: Int = getAdapterPosition()
 
-                Snackbar.make(v,"Click  en el item $position",
-                    Snackbar.LENGTH_LONG).setAction("Action",null).show()
+                Snackbar.make(
+                    v, "Click  en el item $position",
+                    Snackbar.LENGTH_LONG
+                ).setAction("Action", null).show()
             }
+        }
+        override fun onClick(){
+
+            val context: Context = itemView.getContext()
+            val intent = Intent(context, IngresarNotas::class.java)
+            context.startActivity(intent)
+
         }
     }
 
-    override fun onClick(){
 
-    }
 
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
         holder.itemTitle.text= titles[i]
