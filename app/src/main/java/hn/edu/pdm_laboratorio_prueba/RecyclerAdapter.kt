ad1a19dd970c269f552950c222889b418397ea77
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-13 23:37:15 -0600

    nuevo

--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/RecyclerAdapter.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/RecyclerAdapter.kt
@@ -14,53 +14,66 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private val titles = arrayOf("Registrar Alumno",
-            "Matricula", "Registrar Clase", "Ingresar Notas",
-            "Enviar Matricula", "Enviar Nota")
+    private val titles = arrayOf(
+        "Registrar Alumno",
+        "Matricula", "Registrar Clase", "Ingresar Notas",
+        "Enviar Matricula", "Enviar Nota"
+    )
 
-    private val details = arrayOf("Item detalle 1", "Item detalle 2",
-            "Item detalle 3", "Item detalle 4",
-            "Item detalle 5", "Item detalle 6",
-            "Item detalle 7", "Item detalle 8")
+    private val details = arrayOf(
+        "Item detalle 1", "Item detalle 2",
+        "Item detalle 3", "Item detalle 4",
+        "Item detalle 5", "Item detalle 6",
+        "Item detalle 7", "Item detalle 8"
+    )
 
-    private val images = intArrayOf(R.drawable.image2,
+    private val images = intArrayOf(
+        R.drawable.image2,
         R.drawable.imagen1, R.drawable.clases,
         R.drawable.images3, R.drawable.images4,
-        R.drawable.image5)
+        R.drawable.image5
+    )
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
         val v = LayoutInflater.from(viewGroup.context)
-                .inflate(R.layout.card_layout, viewGroup, false)
+            .inflate(R.layout.card_layout, viewGroup, false)
         return ViewHolder(v)
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var itemImage: ImageView
         var itemTitle: TextView
         var itemDetail: TextView
 
         init {
-            itemImage=itemView.findViewById(R.id.item_image)
-            itemTitle=itemView.findViewById(R.id.item_title)
-            itemDetail=itemView.findViewById(R.id.item_detail)
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+            itemDetail = itemView.findViewById(R.id.item_detail)
 
-            itemView.setOnClickListener{ v: View ->
+            itemView.setOnClickListener { v: View? ->
+                val context: Context = itemView.getContext()
+                val intent = Intent(context, IngresarNotas::class.java)
+                context.startActivity(intent)
+
                 var position: Int = getAdapterPosition()
-
-                Snackbar.make(v,"Click  en el item $position",
-                    Snackbar.LENGTH_LONG).setAction("Action",null).show()
+                if (v != null) {
+                    Snackbar.make(v, "Click  en el item $position",
+                        Snackbar.LENGTH_LONG
+                    ).setAction("Action", null).show()
+                }
             }
+
+
         }
     }
 
 
-        override fun onClick(v: View){
 
-            val context: Context = v.getContext()
-            val intent = Intent(context, IngresarNotas::class.java)
-            context.startActivity(intent)
 
-        }
+
+
+
+
 
 
 
