c91d189216f55a417f37ce5f9d6c64f9722f6872
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-13 22:43:49 -0600

    notas

--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/IngresarNotas.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/IngresarNotas.kt
@@ -1,4 +1,10 @@
 package hn.edu.pdm_laboratorio_prueba
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class IngresarNotas : AppCompatActivity() {
 
-class IngresarNotas {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingresar_notas)
+    }
 }
