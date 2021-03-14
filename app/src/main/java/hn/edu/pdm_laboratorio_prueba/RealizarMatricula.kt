3f348697e36ff9fbae00288976cbb3c47dd17395
daniela120 <79028610+daniela120@users.noreply.github.com>
2021-03-14 00:53:10 -0600

    Funciones, boton regresar

--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/RealizarMatricula.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/RealizarMatricula.kt
@@ -1,11 +1,42 @@
 package hn.edu.pdm_laboratorio_prueba
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_realizar_matricula.*
+import kotlinx.android.synthetic.main.activity_registrar_alumno.*
+import java.lang.StringBuilder
 
 class RealizarMatricula : AppCompatActivity() {
+    var datos_Matricula: HashMap<Int, String> = hashMapOf()
+    var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_realizar_matricula)
+        btn_Matricular.setOnClickListener { guardar() }
+        btn_regresar2.setOnClickListener { regresar1() }
     }
+
+    private  fun guardar() {
+        val parametro = StringBuilder()
+        num += 1
+        parametro.append(txt_nCuentaMat.text.toString().trim()).append("|")
+
+        datos_Matricula.put(num, parametro.toString())
+
+    }
+
+    private fun enviar(){
+        val intent = Intent(this, EnviarMatricula ::class.java)
+        intent.putExtra("datos", datos_Matricula)
+        startActivity(intent)
+    }
+
+
+
+    fun regresar1() {
+        val intent = Intent(this, RecyclerAdapter::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/RegistrarAlumno.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/RegistrarAlumno.kt
@@ -1,4 +1,23 @@
 package hn.edu.pdm_laboratorio_prueba
 
-class RegistrarAlumno {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_registrar_alumno.*
+
+class RegistrarAlumno : AppCompatActivity() {
+    var datos: HashMap<Int, String> = hashMapOf()
+    var num = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_alumno)
+        btn_regresar.setOnClickListener { regresar() }
+        }
+
+    fun regresar() {
+        val intent = Intent(this, RecyclerAdapter::class.java)
+        startActivity(intent)
+    }
+
 }
