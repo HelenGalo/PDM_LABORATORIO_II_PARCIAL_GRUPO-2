5365b1070279fac0f3a6aff21cc7a2e0394cf8a4
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-15 16:19:15 -0600

    validaciones

--- app/src/main/java/hn/edu/pdm_laboratorio_prueba/IngresarNotas.kt
+++ app/src/main/java/hn/edu/pdm_laboratorio_prueba/IngresarNotas.kt
@@ -51,17 +51,6 @@ class IngresarNotas : AppCompatActivity() {
 
     private  fun guardar() {
 
-        val parametro = StringBuilder()
-        num += 1
-        parametro.append("DATOS ALUMNOS").append("|")
-        parametro.append(txt_nCuentaA3.text.toString().trim()).append("|")
-        parametro.append(txt_NombreA2.text.toString().trim()).append("|")
-        parametro.append(spinner_NClase2.selectedItem.toString().trim()).append("|")
-        parametro.append(txt_Nota.text.toString().trim()).append("|")
-
-        datos_nota.put(num, parametro.toString())
-        println(datos_nota.toString())
-
         if (txt_nCuentaA3.text.toString().isEmpty()) {
             Toast.makeText(this, "Debe de ingresar un Numero de cuenta", Toast.LENGTH_SHORT).show()
         }else {
@@ -72,6 +61,17 @@ class IngresarNotas : AppCompatActivity() {
                     Toast.makeText(this, "Ingrese una nota", Toast.LENGTH_SHORT).show()
                 }else{
                     Toast.makeText(this, "Nota Registrada!", Toast.LENGTH_SHORT).show()
+
+                    val parametro = StringBuilder()
+                    num += 1
+                    parametro.append("DATOS ALUMNOS").append("|")
+                    parametro.append(txt_nCuentaA3.text.toString().trim()).append("|")
+                    parametro.append(txt_NombreA2.text.toString().trim()).append("|")
+                    parametro.append(spinner_NClase2.selectedItem.toString().trim()).append("|")
+                    parametro.append(txt_Nota.text.toString().trim()).append("|")
+
+                    datos_nota.put(num, parametro.toString())
+                    println(datos_nota.toString())
                 }
 
             }
